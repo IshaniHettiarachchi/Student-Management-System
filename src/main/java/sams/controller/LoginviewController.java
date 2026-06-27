89aa00bf3d31742a600188e81eb63007c815a3ae
Ishani Hettiarachchi <ishanihettiarachchi37@gmail.com>
2026-06-27 13:52:04 +0530

    dev login

---
+++ src/main/java/sams/controller/LoginviewController.java
@@ -0,0 +1,34 @@
+package sams.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginviewController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label lblPswrd;
+
+    @FXML
+    private Label lblText;
+
+    @FXML
+    private Label lblUser;
+
+    @FXML
+    private TextField txtPswrd;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
